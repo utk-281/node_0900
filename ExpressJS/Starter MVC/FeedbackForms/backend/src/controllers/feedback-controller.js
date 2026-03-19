68e04b7b5e36e4f060b55448376adff7f3a9abce
utkarsh <utkarshgupta.281@gmail.com>
2026-03-19 11:07:50 +0530

    add basic error handling

--- ExpressJS/Starter MVC/FeedbackForms/backend/src/controllers/feedback-controller.js
+++ ExpressJS/Starter MVC/FeedbackForms/backend/src/controllers/feedback-controller.js
@@ -10,14 +10,92 @@ export const submitFeedback = async (req, res, next) => {
       payload: newFeedback,
     });
   } catch (error) {
+    // return res.status(500).json({
+    //   success: false,
+    //   message: "Error while creating a Feedback",
+    //   myErr: error,
+    // });
+    if (error.name === "ValidationError") {
+      return res.status(400).json({
+        success: false,
+        message: error.message,
+      });
+    }
+    // else if(){} try to handle duplicate error object from mongoose (code)
+  }
+};
+
+export const getFeedbacks = async (req, res, next) => {
+  try {
+    let feedbacks = await FeedbackModel.find();
+    // console.log("feedbacks: ", feedbacks);
+
+    if (feedbacks.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "No feedbacks found!!",
+      }); //TODO: later on we will throw an error;
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "All feedbacks fetched successfully",
+      totalNumber: feedbacks.length,
+      payload: feedbacks,
+    });
+  } catch (error) {
     console.log(error);
   }
 };
 
-export const getFeedbacks = (req, res, next) => {};
+export const getFeedback = async (req, res, next) => {
+  let { feedbackId } = req.params;
+  // console.log("feedbackId: ", feedbackId);
+  let feedback = await FeedbackModel.findById(feedbackId);
+  res.status(200).json({
+    success: true,
+    message: "Feedback fetched successfully",
+    payload: feedback,
+  });
+};
+//? params -> parameters
 
-export const getFeedback = (req, res, next) => {};
+export const updateFeedback = async (req, res, next) => {
+  try {
+    // console.log(req.params);
+    let updatedData = await FeedbackModel.findByIdAndUpdate(
+      req.params.feedbackId,
+      req.body,
+    );
 
-export const updateFeedback = (req, res, next) => {};
+    res.status(200).json({
+      message: "Updated Successfully",
+      success: true,
+      payload: updatedData,
+    });
+    //? findByIdAndUpdate(filter by _id, req.body, options)
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export const deleteFeedback = (req, res, next) => {};
+export const deleteFeedback = async (req, res, next) => {
+  try {
+    let feedbackId = req.params.feedbackId;
+    let deletedFeedback = await FeedbackModel.findByIdAndDelete(feedbackId);
+    res.status(200).json({
+      message: "Deleted Successfully",
+      success: true,
+      payload: deletedFeedback,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// http://localhsot:9000/api/one/8902309umkj?key=value&key2=valuw2
+//? http://localhsot:9000 -> base url
+//? /api -> api version
+//? /one -> endpoint
+//? 8902309u -> params (req.params)
+//? after ? everything comes under query params (req.query)
--- ExpressJS/Starter MVC/FeedbackForms/backend/src/routes/feedback-routes.js
+++ ExpressJS/Starter MVC/FeedbackForms/backend/src/routes/feedback-routes.js
@@ -1,11 +1,38 @@
 import { Router } from "express";
 
-import { submitFeedback } from "../controllers/feedback-controller.js";
+import {
+  deleteFeedback,
+  getFeedback,
+  getFeedbacks,
+  submitFeedback,
+  updateFeedback,
+} from "../controllers/feedback-controller.js";
 
 const router = Router();
 
 router.post("/submit", submitFeedback);
 
+router.get("/all", getFeedbacks);
+
+router.get("/one/:feedbackId", getFeedback);
+//? ":id" -> dynamic routing
+
+router.patch("/edit/:feedbackId", updateFeedback);
+
+router.delete("/delete/:feedbackId", deleteFeedback);
+
 export default router;
 
-/// http://localhost:9000/API_VERSION/ENDPOINT
+/// http://localhost:9000/API_VERSION/one/89asdhasohkjnak
+
+//~ put -> it is used to update the entire resource
+//~ patch -> it is used to update partial resource
+
+let data = {
+  name: "a",
+  age: "34",
+  email: "a@gmail.com",
+};
+
+//? updating age with put -> req.body.age --> {age:35}
+//? updating age with patch -> req.body.age --> {age:35, name:"a", email:""}
